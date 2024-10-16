78bb0016b0c1d0f29e248c3a15c6d79275fbc78e
Florian <f.rumigny@gmail.com>
2024-10-16 19:26:41 +0200

    session log duration features

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Pause, Play } from "lucide-react";
+import { Pause, Play, TimerReset } from "lucide-react";
 
 function App() {
-  const [clock, setClock] = useState<number>(0);
+  const [clock, setClock] = useState<number>(24);
   const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [isOnBreak, setIsOnBreak] = useState<boolean>(false);
+  const [logDuration, setLogDuration] = useState<number>(0);
+  const [logBreaks, setLogBreaks] = useState<number>(0);
 
-  // const seconds = 1;
-  // const minutes = seconds * 60;
+  const hours = new Date().getHours().toString();
+  const minutes = new Date().getMinutes().toString().padStart(2, "0");
+  const today = new Date();
+
+  const dayOfWeek = new Intl.DateTimeFormat("fr-FR", {
+    weekday: "long",
+  }).format(today);
+
+  const clockDay = `${hours}:${minutes}`;
+
+  const sayHello = () => {
+    const trueHours = new Date().getHours();
+
+    let greetings = "";
+
+    switch (true) {
+      case trueHours < 5:
+        return (greetings = "Good Night");
+      case trueHours < 12:
+        return (greetings = "Good Morning");
+      case trueHours < 18:
+        return (greetings = "Good Afternoon");
+      case trueHours < 23:
+        return (greetings = "Good Evening");
+      default:
+        return (greetings = "Good Night");
+    }
+
+    return greetings;
+  };
 
   const handleStart = () => {
     setIsRunning(true);
@@ -17,20 +48,24 @@ function App() {
     setIsRunning(false);
   };
 
+  const handleReset = () => {
+    setClock(0);
+    setIsRunning(false);
+  };
+
   useEffect(() => {
     let intervalId: number | null = null;
     const startClock = () => {
       if (isRunning && clock < 25) {
         intervalId = setInterval(() => {
           setClock((prevTime) => prevTime + 1);
+          setLogDuration((prevTime) => prevTime + 1);
           console.log(isRunning);
         }, 60000);
-      } else {
-        setIsRunning(false);
+      } else if (clock > 25) {
+        handleStop();
+        setIsOnBreak(true);
       }
-      // en gros je set interval à 1 minute (en milliseconde) de set clock, d'1 minute chaque minute
-      // quand le compteur est à 25, je stop ?
-      // si le bouton (donc il faudra un useState boolean) stop est appuyé, je stop. Si le bouton play est réappuyé et que les 25 ne sont pas passés, ça reprend ?
     };
 
     startClock();
@@ -45,25 +80,48 @@ function App() {
         className={isRunning ? "container-main is-on" : "container-main is-off"}
       >
         <div className="container-title">
+          <div>
+            <p className="title-hello">{sayHello()}</p>
+            <p className="title-calendar">
+              {" "}
+              {dayOfWeek} {clockDay}
+            </p>
+          </div>
           <h1 className="title-clock">
             {clock} {clock > 1 ? "minutes" : "minute"}
           </h1>
         </div>
         <div className="container-button">
           <button
+            onClick={handleReset}
+            type="button"
+            className="btn button-reset"
+          >
+            <TimerReset size={20} />
+          </button>
+          <button
             onClick={handleStart}
             type="button"
+            disabled={isRunning}
             className="btn button-start"
           >
-            <Play size={20} /> Clock in
+            <Play size={20} />
           </button>
           <button
             onClick={handleStop}
             type="button"
             className="btn button-stop"
           >
-            <Pause size={20} /> Break time
+            <Pause size={20} />
           </button>
+        </div>
+        {isOnBreak && <p>Vous êtes en pause pour 5 minutes</p>}
+        <div className="container-session">
+          <h2 className="session-title">Session</h2>
+          <p>Duration</p>
+          <p>{logDuration}m</p>
+          <p>Breaks</p>
+          <p>{logBreaks}m</p>
         </div>
       </div>
     </main>
