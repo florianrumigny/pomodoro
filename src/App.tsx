71b13e09e945e5b23f8461758fc109f68a6541b3
Florian <f.rumigny@gmail.com>
2024-10-16 21:00:02 +0200

    feature: break 5 minutes and restart pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   const [logDuration, setLogDuration] = useState<number>(0);
   const [logBreaks, setLogBreaks] = useState<number>(0);
 
+  const fiveMinutes = 5 * 60 * 1000;
+
   const hours = new Date().getHours().toString();
   const minutes = new Date().getMinutes().toString().padStart(2, "0");
   const today = new Date();
@@ -46,6 +48,7 @@ function App() {
 
   const handleStop = () => {
     setIsRunning(false);
+    setIsOnBreak(true);
   };
 
   const handleReset = () => {
@@ -53,18 +56,26 @@ function App() {
     setIsRunning(false);
   };
 
+  const handleBreak = () => {
+    setIsOnBreak(false);
+    handleStart();
+    handleReset();
+  };
+
   useEffect(() => {
     let intervalId: number | null = null;
+
     const startClock = () => {
-      if (isRunning && clock < 25) {
+      if (isRunning && !isOnBreak && clock < 25) {
         intervalId = setInterval(() => {
           setClock((prevTime) => prevTime + 1);
           setLogDuration((prevTime) => prevTime + 1);
           console.log(isRunning);
         }, 60000);
-      } else if (clock > 25) {
-        handleStop();
+      } else if (clock === 25 && !isOnBreak) {
+        setIsRunning(false);
         setIsOnBreak(true);
+        if (intervalId) clearInterval(intervalId);
       }
     };
 
@@ -72,58 +83,86 @@ function App() {
     return () => {
       if (intervalId) clearInterval(intervalId);
     };
-  }, [clock, isRunning]);
+  }, [clock, isRunning, isOnBreak]);
+
+  useEffect(() => {
+    let breakTimeoutId: number | null = null;
+
+    if (isOnBreak) {
+      breakTimeoutId = setTimeout(() => {
+        setIsOnBreak(false);
+        setIsRunning(true);
+        setClock(0);
+        setLogBreaks((prevBreaks) => prevBreaks + 5);
+      }, fiveMinutes);
+    }
+
+    return () => {
+      if (breakTimeoutId) clearTimeout(breakTimeoutId); // Clear timeout on cleanup
+    };
+  }, [isOnBreak, fiveMinutes]);
 
   return (
     <main className="container">
-      <div
-        className={isRunning ? "container-main is-on" : "container-main is-off"}
-      >
-        <div className="container-title">
-          <div>
-            <p className="title-hello">{sayHello()}</p>
-            <p className="title-calendar">
-              {" "}
-              {dayOfWeek} {clockDay}
-            </p>
+      {!isOnBreak ? (
+        <div
+          className={
+            isRunning ? "container-main is-on" : "container-main is-off"
+          }
+        >
+          <div className="container-title">
+            <div>
+              <p className="title-hello">{sayHello()}</p>
+              <p className="title-calendar">
+                {" "}
+                {dayOfWeek} {clockDay}
+              </p>
+            </div>
+            <h1 className="title-clock">
+              {clock} {clock > 1 ? "minutes" : "minute"}
+            </h1>
           </div>
-          <h1 className="title-clock">
-            {clock} {clock > 1 ? "minutes" : "minute"}
-          </h1>
+          <div className="container-button">
+            <button
+              onClick={handleReset}
+              type="button"
+              className="btn button-reset"
+            >
+              <TimerReset size={20} />
+            </button>
+            <button
+              onClick={handleStart}
+              type="button"
+              disabled={isRunning}
+              className="btn button-start"
+            >
+              <Play size={20} />
+            </button>
+            <button
+              onClick={handleStop}
+              type="button"
+              className="btn button-stop"
+            >
+              <Pause size={20} />
+            </button>
+          </div>
+          {isOnBreak && <p>Vous êtes en pause pour 5 minutes</p>}
+          <div className="container-session">
+            <h2 className="session-title">Session</h2>
+            <p>Duration</p>
+            <p>{logDuration}m</p>
+            <p>Breaks</p>
+            <p>{logBreaks}m</p>
+          </div>
         </div>
-        <div className="container-button">
-          <button
-            onClick={handleReset}
-            type="button"
-            className="btn button-reset"
-          >
-            <TimerReset size={20} />
-          </button>
-          <button
-            onClick={handleStart}
-            type="button"
-            disabled={isRunning}
-            className="btn button-start"
-          >
-            <Play size={20} />
-          </button>
-          <button
-            onClick={handleStop}
-            type="button"
-            className="btn button-stop"
-          >
-            <Pause size={20} />
+      ) : (
+        <div>
+          <p>You're on a break, relax.</p>
+          <button onClick={handleBreak} className="btn">
+            Stop break
           </button>
         </div>
-        {isOnBreak && <p>Vous êtes en pause pour 5 minutes</p>}
-        <div className="container-session">
-          <h2 className="session-title">Session</h2>
-          <p>Duration</p>
-          <p>{logDuration}m</p>
-          <p>Breaks</p>
-          <p>{logBreaks}m</p>
-        </div>
-      </div>
+      )}
     </main>
   );
 }
